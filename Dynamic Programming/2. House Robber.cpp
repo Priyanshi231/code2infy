a74949f507e35c3dadcf7059826e3fda0b579c29
Priyanshi Jain <163301165+Priyanshi231@users.noreply.github.com>
2026-09-03 23:47:51 +0530

    Implement memory optimization for House Robber solution
    Added a memory-optimized solution for the House Robber problem.

--- Dynamic Programming/2. House Robber.cpp
+++ Dynamic Programming/2. House Robber.cpp
@@ -1,3 +1,5 @@
+// 2 solutions
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -16,5 +18,31 @@ public:
         }
         
         return dp[n-1];
+    }
+};
+
+
+// memory optimization
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n==1){
+            return nums[0];
+        }
+
+        int pre1 = nums[0];
+        int pre2 = max(nums[0], nums[1]);
+        int res = pre2;
+
+        for(int i=2; i<n; i ++){
+            res = max(pre1 + nums[i], pre2);
+            pre1 = pre2;
+            pre2 = res;
+        }
+        
+        return res;
     }
 };
